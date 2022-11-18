919b68e4d9cd447511cba280911165d9612d711f
Garlo <108318571+happygaaro@users.noreply.github.com>
2022-11-18 14:42:03 +1100

    Update streamlit_app.py
    filtering the table data with new object 'fruits to show'

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,8 +12,10 @@ st.text('🐔Hard-Boiled Free-Range Egg')
 st.text('🥑🍞Avocado Toast')
 
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
+# Lets put a pick list here so they can pick the fruit they want to include
 st.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado', 'Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-st.dataframe(my_fruit_list)
+#Display the table on the page
+st.dataframe(fruits_to_show)
 
