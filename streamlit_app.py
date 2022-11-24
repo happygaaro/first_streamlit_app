40a9b2db0e5848bdb205d4d4506bdbd56fcf209c
Garlo <108318571+happygaaro@users.noreply.github.com>
2022-11-24 16:26:16 +1100

    Update streamlit_app.py
    added error handling

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,18 +26,17 @@ st.dataframe(fruits_to_show)
 
 
 st.header("Fruityvice Fruit Advice!")
+try:
+  fruit_choice = st.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    st.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    st.dataframe(fruityvice_normalized)
 
-fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
-st.write('The user entered ', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-
-
-
-# write your own comment -what does the next line do? it makes it more readable into a table format
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do? displays the normalised view
-st.dataframe(fruityvice_normalized)
-
+except URLError as e:
+  st.error()
 
 #don't run anyuthing past here while we trouobleshoot
 st.stop()
